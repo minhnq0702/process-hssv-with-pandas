b01a51d8b71a9528746404ac308c4ebf3b46449d
MingNe <minhnq.0702@gmail.com>
2024-04-23 14:47:27 +0700

    Update source

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 # -*- coding: utf-8 -*-
-# Copyright 2022 Init, Ltd (https://www.init.vn/)
-import pandas
+import os
+import logging
 import pandas as pd
 
-if __name__ == '__main__':
-    hssv = pd.read_excel('hssv.xlsx', sheet_name='TH-7777', header=0)
-    noi_dung = hssv['Nội dung']
 
-    noi_dung = noi_dung.astype('string')
-    # noi_dung = noi_dung.str.replace(',', ' ').str.replace('-', ' ').str.replace('_', ' ').str.replace(';', ' ').str.replace('.', ' ')
-    noi_dung = noi_dung.str.replace("[^0-9:]+", ' ', regex=True)
-    render_mssv = []
-    for items in noi_dung:
-        split_items = items.split(' ')
-        mssvs = ', '.join(list(filter(lambda i: len(i) == 8 and i.isnumeric(), split_items)))
-        render_mssv.append(mssvs)
-    final = hssv.assign(MSSV=pandas.Series(render_mssv))
-    final.to_excel('final-hssv.xlsx')
+_logger = logging.getLogger(__name__)
+OUTPUT_DIR = './data'
+
+
+def get_input_file_name():
+    # Get input file name from user input in terminal
+    input_file_name = input('Enter the input file name: ')
+    return input_file_name
+
+
+if __name__ == '__main__':
+    filename = get_input_file_name()
+    df = pd.read_excel(filename, sheet_name='Sheet1', header=0)
+    content = df['Nội dung']
+
+    content = content.astype('string')
+    # noi_dung = (
+    #     content.str.replace(',', ' ')
+    #     .str.replace('-', ' ')
+    #     .str.replace('_', ' ')
+    #     .str.replace(';', ' ')
+    #     .str.replace('.', ' ')
+    # )
+    content = content.str.replace("[^0-9:]+", ' ', regex=True)
+    render_ids = []
+    for items in content:
+        try:
+            split_items = items.split(' ')
+            ids = ', '.join(list(filter(
+                lambda i: len(i) == 8 and i.isnumeric(), split_items))
+            )
+            render_ids.append(ids)
+        except Exception as e:
+            render_ids.append('')
+            _logger.error(f'Can not get ID with  error :{e}')
+    final = df.assign(Data=pd.Series(render_ids))
+    output_file_dir = os.path.join(OUTPUT_DIR, 'final.xlsx')
+    final.to_excel(output_file_dir)
